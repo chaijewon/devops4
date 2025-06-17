1f4a48dde05029476859a8a1498fe81df898b6dc
chaijewon <vcandjava@nate.com>
2025-06-17 14:02:12 +0900

    1111

--- src/main/java/com/sist/web/Devops4Application.java
+++ src/main/java/com/sist/web/Devops4Application.java
@@ -2,7 +2,7 @@ package com.sist.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Hello
 @SpringBootApplication
 public class Devops4Application {
 
