33164f517d4e8e2c00737a1d3006567a6a7bac1e
chaijewon <vcandjava@nate.com>
2025-06-17 12:47:08 +0900

    1111

--- src/main/java/com/sist/web/Devops4Application.java
+++ src/main/java/com/sist/web/Devops4Application.java
@@ -3,9 +3,6 @@ package com.sist.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-
 @SpringBootApplication
 public class Devops4Application {
 
--- src/main/java/com/sist/web/controller/MainController.java
+++ src/main/java/com/sist/web/controller/MainController.java
@@ -11,6 +11,43 @@ import com.sist.web.vo.*;
 import com.sist.web.service.*;
 
 @Controller
+/*
+ *    docker 
+ *     => 가상머신처럼 => 독립적인 실행환경을 만들어주는 => 운영체제 
+ *        파일을 이미지화 => 저장하는 용도 
+ *     => 1. swap
+ *     => 2. update 
+ *     => 3. root 계정 
+ *     => 4. java 설치 
+ *     => 5. docker 설치 
+ *           => 도구 설치 curl ...
+ *     => 6. docker hub 등록 
+ *     => 7. 저장=> docker hub에 이미지를 저장 
+ *     => 8. docker-compose에 등록 => git Actions에서 전송
+ *        --------------------------------------------- 우분투와 통신할 SSH 
+ *        => ed25519 
+ *     => 필요시마다 docker hub에서 출력 
+ *     
+ *     1. Image : => 프로그램 => read-only 
+ *     2. Dockerfile : 도커 이미지를 만든는 설명서 
+ *     3. Container : 프로세스 => 휘발성 => 메모리에 상주 
+ *     4. 저장  => Docker Hub에 주로 저장  
+ *     
+ *     AWS 
+ *       => ubuntu => root => sudo su -
+ *       => root : => ubuntu가 사용 가능하게 ...
+ *                    usemode -a -G docker ubuntu
+ *       => docker version => 1.7이상 
+ *          상태 확인 
+ *            systemctl status docker 
+ *          8080 포트 동작 확인 
+ *          netstat -tnlp | grep 8080 
+ *          => kill -9 id명(PID) 
+ *          
+ *       => 도커 종료   systemctl stop docker
+ *       => 도커 실행   systemctl start docker 
+ *       => docker enabled
+ */
 public class MainController {
    @Autowired
    private FoodService fService;
