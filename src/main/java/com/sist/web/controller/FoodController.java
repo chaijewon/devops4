0d46cc33a645d465248361cd85c10210280cf710
chaijewon <vcandjava@nate.com>
2025-06-17 16:27:04 +0900

    1111

--- src/main/java/com/sist/web/controller/FoodController.java
+++ src/main/java/com/sist/web/controller/FoodController.java
@@ -3,6 +3,7 @@ package com.sist.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 // 전송 => 브라우저
+
 @Controller
 public class FoodController {
  
